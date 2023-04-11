455aff21d750e0997a2ee0a4823b80c19a499306
Titus Chu <tituschu@uchicago.edu>
2023-04-11 14:41:06 +0000

    finished

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -12,3 +12,16 @@ class NumbersController < ApplicationController
   end 
 end
 
+class NumbersController < ApplicationController
+  def losers
+    @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+      
+      @zebra.push(giraffe)
+    end
+
+    render({ :template => "lottery_stuff/bad.html.erb"})
+  end 
+end
